f1ccab558b5d635135ad81786e58fd298217d942
fsilv <fsilv@>
2018-12-30 13:59:19 +0000

    [FS 20181230] - Adicao de ficheiros html, css, js ao projecto.

---
+++ com.webapp/src/main/java/com/webapp/config/ThymeleafConfig.java
@@ -0,0 +1,16 @@
+package com.webapp.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+public class ThymeleafConfig {
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//	    registry.addResourceHandler("/css/**").addResourceLocations("/css/**");
+//	    registry.addResourceHandler("/img/**").addResourceLocations("/img/**");
+//	    registry.addResourceHandler("/js/**").addResourceLocations("/js/**");
+//	    registry.addResourceHandler("/sound/**").addResourceLocations("/sound/**");
+//	    registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/**");
+//	}
+	
+}
